de5b1dc5cdae7552ab6439ba12fa849c546da5ce
elleklinton <ellek.linton@gmail.com>
2023-05-04 02:18:37 -0700

    new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://elleklinton.github.io',
+  site: 'https://glamazonwebservices.com',
   base: '/',
   server: {
     port: 8000

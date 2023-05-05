1fb873284266d8e0da5c879066f66f29115fe5d3
elleklinton <ellek.linton@gmail.com>
2023-05-05 00:01:18 -0700

    add new ai generaed icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,17 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/">Ellek's E-Commerce Store</a>
-        <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon mt-2">
-            <span className="navbar-toggler-bar bar1"></span>
-            <span className="navbar-toggler-bar bar2"></span>
-            <span className="navbar-toggler-bar bar3"></span>
-          </span>
-        </button>
+        <a className="navbar-brand container ms-lg-0 flex" href="/">
+            <img src="/logo-sm.png" alt="logo" className='icon-sm'/>
+            <h4 className="pad-left">Ellek's Store</h4>
+        </a>
+        {/*<button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">*/}
+        {/*  <span className="navbar-toggler-icon mt-2">*/}
+        {/*    <span className="navbar-toggler-bar bar1"></span>*/}
+        {/*    <span className="navbar-toggler-bar bar2"></span>*/}
+        {/*    <span className="navbar-toggler-bar bar3"></span>*/}
+        {/*  </span>*/}
+        {/*</button>*/}
         <div className="collapse navbar-collapse" id="navigation">
           <ul className="navbar-nav ms-auto">
             {/*<li className="nav-item">*/}
--- src/components/store/storeDoubleColumn.tsx
+++ src/components/store/storeDoubleColumn.tsx
@@ -6,7 +6,7 @@ export default function StoreNavigationDoubleColumn() {
         <h6 className="border-bottom">© 2023 Ellek Linton</h6>
         <div className='text-sm'>
           <i>
-            Disclaimer: all user-facing text on this website, including product features, highlights, descriptions, reviews, and even this message, were written by ChatGPT - a highly intelligent language model trained by OpenAI. While we believe that ChatGPT has excellent taste and a great sense of humor, its opinions may not always align with those of human beings. We apologize in advance for any unintended hilarity, confusion, or existential crises that may arise from interacting with our AI friends. Please use at your own risk!
+            Disclaimer: all user-facing text on this website, including product features, highlights, descriptions, reviews, and even this message, were written by ChatGPT - a highly intelligent language model trained by OpenAI. The website logo was generated with MidJourney AI. While we believe that ChatGPT has excellent taste and a great sense of humor, its opinions may not always align with those of human beings. We apologize in advance for any unintended hilarity, confusion, or existential crises that may arise from interacting with our AI friends. Please use at your own risk!
             <br />
             <br />
             This is a simulated e-commerce store for testing and educational purposes only. No actual payments or transactions will be processed. All purchases made on this website are for demonstration purposes only.

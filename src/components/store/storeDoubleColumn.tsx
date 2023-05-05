3cea164c64324becde68c2bb60181b8cac34cfa8
elleklinton <ellek.linton@gmail.com>
2023-05-04 22:19:52 -0700

    Added disclaimer

--- src/components/store/storeDoubleColumn.tsx
+++ src/components/store/storeDoubleColumn.tsx
@@ -2,129 +2,22 @@ export default function StoreNavigationDoubleColumn() {
   return (
     <>
     <div className="row mt-5">
-      <div className="col-12 col-lg-6 mb-4 mb-lg-0">
-        <h6 className="w-100 pb-3 border-bottom">Clothing</h6>
-        <div className="d-flex pt-2">
-          <ul className="nav flex-column ms-n3">
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Tops
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Dresses
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Pants
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Denim
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Sweaters
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                T-Shirts
-              </a>
-            </li>
-          </ul>
-          <ul className="nav flex-column ms-6 ms-md-11 ms-lg-10">
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Jackets
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Activewear
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Shorts
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Swimwear
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Browse All
-              </a>
-            </li>
-          </ul>
+      <div>
+        <h6 className="border-bottom">© 2023 Ellek Linton</h6>
+        <div className='text-sm'>
+          <i>
+            Disclaimer: all user-facing text on this website, including product features, highlights, descriptions, reviews, and even this message, were written by ChatGPT - a highly intelligent language model trained by OpenAI. While we believe that ChatGPT has excellent taste and a great sense of humor, its opinions may not always align with those of human beings. We apologize in advance for any unintended hilarity, confusion, or existential crises that may arise from interacting with our AI friends. Please use at your own risk!
+            <br />
+            <br />
+            This is a simulated e-commerce store for testing and educational purposes only. No actual payments or transactions will be processed. All purchases made on this website are for demonstration purposes only.
+            <br />
+            <br />
+            This website was created by Ellek Linton using the open-source <a target="_blank" href="https://github.com/creativetimofficial/soft-ui-dashboard/blob/main/LICENSE">Astro Ecommerce</a> template, under the <a target="_blank" href="https://github.com/creativetimofficial/soft-ui-dashboard/blob/main/LICENSE">MIT License</a>.
+            <br />
+            <br />
+            :-)
+          </i>
         </div>
-      </div>
-      <div className="col-6 col-lg-3">
-        <h6 className="w-100 pb-3 border-bottom">Accessories</h6>
-        <ul className="nav flex-column ms-n3 pt-2">
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Shoes
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Jewelry
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Handbags
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Socks
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Hats
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Browse All
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="col-6 col-lg-3">
-      <h6 className="w-100 pb-3 border-bottom ">Categories</h6>
-        <ul className="nav flex-column ms-n3 pt-2">
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              New Arrivals
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Sale
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Basic Tee
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Artwork Tees
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
     </>
